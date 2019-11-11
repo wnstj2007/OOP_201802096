6e345fe955a277dcbca48cf3470d897bb9167f68
wnstj2007 <201802096@o.cnu.ac.kr>
2019-11-12 01:38:54 +0900

    10주차 과제2 구현
    201802096 박준서

--- week10/smart_obj-hw/human.cpp
+++ week10/smart_obj-hw/human.cpp
@@ -7,21 +7,23 @@
 human::human() {
     // TODO: Fill it
     // name 의 default 값을 nullptr로 지정
+    name = nullptr;
 }
 
 human::human(char name[]) {
     // TODO: Fill it
     // name 의 값을 지정
+    this->name = name;
 }
 
 char *human::get_name() {
     // TODO: Fill it
     // name 의 값을 반환하게 구현
-    return nullptr;
+    return this->name;
 }
 
 int human::get_id() {
     // TODO: Fill it
     // 0을 리턴하도록 구현
-    return -1;
+    return 0;
 }
--- week10/smart_obj-hw/shared_ref.cpp
+++ week10/smart_obj-hw/shared_ref.cpp
@@ -8,6 +8,7 @@ namespace ptr {
 shared_ref::shared_ref() {
     // TODO: Fill it
     // _mgr의 default 값을 빈 mgr으로 설정
+    _mgr = mgr();
 }
 
 shared_ref::shared_ref(student* student) {
@@ -15,17 +16,21 @@ shared_ref::shared_ref(student* student) {
     // ptr_map에 student의 주소가 있으면 _mgr을 빈 mgr으로 설정
     // ptr_map에 student의 주소가 없으면 _mgr을 student의 메모리를 관리하는 mgr으로 설정하고
     // ptr_map에 key를 student의 주소로 하고 value를 mgr로 등록 ==> student의 주소가 shared_ref의 mgr로 관리되는지 확인 가능
+    
 }
 
 shared_ref::shared_ref(const shared_ref& shared) {
     // TODO: Fill it
     // shared_ref 의 _mgr을 자신의 _mgr로 설정
     // reference가 증가하였으므로 increase 호출
+	this->_mgr = shared._mgr;
+	increase();
 }
 
 shared_ref::~shared_ref() {
     // TODO: Fill it
     // mgr을 반환하는 release를 호출하게 만듦
+	release();
 }
 
 void shared_ref::release() {
@@ -34,12 +39,21 @@ void shared_ref::release() {
     // _mgr이 nullptr가 아닐 경우 _mgr의 count를 감소시키고
     // count가 0인 경우 ptr_map에서 _mgr의 ptr가 key인 value를 nullptr로 설정하고 _mgr을 delete함
     // ==> student 포인터에 대해 mgr 관리가 끝났음을 알려줌
+	if(_mgr) {
+		_mgr->count--;
+		if(_mgr->count == 0) {
+			
+		}
+	}
 }
 
 student *shared_ref::get() const {
     // TODO: Fill it
     // _mgr이 nullptr일 경우 nullptr 반환하고
     // _mgr이 nullptr가 아닐 경우 _mgr의 ptr을 return 하도록 구현
+	if(_mgr) {
+		return _mgr;
+	}
 
     return nullptr;
 }
@@ -48,6 +62,9 @@ void shared_ref::increase() {
     // TODO: Fill it
     // _mgr이 nullptr일 경우 아무런 작업도 하지 않음
     // _mgr이 nullptr가 아닐 경우 _mgr의 count를 증가시킴
+	if(_mgr) {
+		_mgr->count++;
+	}
 }
 
 shared_ref &shared_ref::operator=(const shared_ref &r) {
@@ -56,13 +73,22 @@ shared_ref &shared_ref::operator=(const shared_ref &r) {
     // ex: a = a;
     // r의 주소가 this와 다른 경우 현재 _mgr을 release하고 r의 _mgr을 this의 _mgr로 설정. 그 후 increase
     // ex: b = a; -> a의 값을 가리키는 것이 a와 b 두 값이 됨
-    return *this;
+	if(this != &r) {
+		this->release();
+		this->_mgr = r.get();
+		this->increase();
+	}
+
+    	return *this;
 }
 
 student *shared_ref::operator->() {
     // TODO: Fill it
     // _mgr이 nullptr일 경우 nullptr 반환
     // _mgr이 nullptr가 아닐 경우 _mgr의 ptr을 반환
+	if(_mgr) {
+		return _mgr;
+	}
 
     return nullptr;
 }
@@ -71,6 +97,9 @@ int shared_ref::count() {
     // TODO: Fill it
     // _mgr이 nullptr일 경우 0을 반환
     // _mgr이 nullptr가 아닐 경우 _mgr의 count를 반환
+	if(_mgr) {
+		return _mgr->count;
+	}
 
     return 0;
 }
